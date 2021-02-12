9af219cc2b2c1e3aa72e918ba85baf9f2590af92
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-02-12 23:48:41 +0900

    Update orbit_diagram.py

--- orbit_diagram.py
+++ orbit_diagram.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import time
 from scipy import optimize
 from scipy import integrate
+import pandas as pd
 
-def make_orbit_diagram(xdot_fun, x0, t, R):
+def make_orbit_diagram(xdot_fun, x0, t, R, return_bins = False):
     """
     Makes the orbit diagram for some 3D ODE system.
-
     Inputs
     ------
     xdot_fun: function
@@ -18,7 +18,6 @@ def make_orbit_diagram(xdot_fun, x0, t, R):
         np.array of time steps to integrate over
     R: np.array
         np.array of ODE parameters to vary in order to create the orbit diagram
-
     Returns
     -------
     X, Y, Z: list
@@ -32,6 +31,11 @@ def make_orbit_diagram(xdot_fun, x0, t, R):
     Y = []
     Z = []
 
+    if return_bins == True:
+        X_bins = []
+        Y_bins = []
+        Z_bins = []
+
     for i in range(len(R)):
         # integrate the ODE and get the steady state solution for each R
         r = R[i]
@@ -43,45 +47,70 @@ def make_orbit_diagram(xdot_fun, x0, t, R):
         y = a[start_index:, 1]
         z = a[start_index:, 2]
 
-        X_max = []
-        Y_max = []
-        Z_max = []
-        X_min = []
-        Y_min = []
-        Z_min = []
-
-        # as we evaluate the local maximum and minimum using 5 points, 
-        # we leave out the first and last 2 elements
-        for count in range(2, len(z)-2, 1):
-            # for each index, check if x[count] is the local maximum/minimum, 
-            # and update the previous and current values for all 3 variables
-            if x[count-2] < x[count-1] < x[count] > x[count+1] > x[count+2]:
-                X_max.append(x[count])
-            if x[count-2] > x[count-1] > x[count] < x[count+1] < x[count+2]:
-                X_min.append(x[count])
-            if y[count-2] < y[count-1] < y[count] > y[count+1] > y[count+2]:
-                Y_max.append(y[count])
-            if y[count-2] > y[count-1] > y[count] < y[count+1] < y[count+2]:
-                Y_min.append(y[count])
-            if z[count-2] < z[count-1] < z[count] > z[count+1] > z[count+2]:
-                Z_max.append(z[count])
-            if z[count-2] > z[count-1] > z[count] < z[count+1] < z[count+2]:
-                Z_min.append(z[count])
-
-        X_max = X_max + X_min
-        Y_max = Y_max + Y_min
-        Z_max = Z_max + Z_min
+        X_max, X_min, Y_max, Y_min, Z_max, Z_min = _make_orbit_diagram(x, y, z)
+        X_max = np.hstack([X_max, X_min])
+        Y_max = np.hstack([Y_max, Y_min])
+        Z_max = np.hstack([Z_max, Z_min])
 
         X.append(X_max)
         Y.append(Y_max) 
         Z.append(Z_max)
 
-    return X, Y, Z
+        if return_bins == True:
+            X_bins.append(get_bins(np.hstack([X_max, X_min])))
+            Y_bins.append(get_bins(np.hstack([Y_max, Y_min])))
+            Z_bins.append(get_bins(np.hstack([Z_max, Z_min])))
 
-def plot_orbit_diagram(R, Z):
+    if return_bins == True:
+        return X, Y, Z, X_bins, Y_bins, Z_bins
+    else:
+        return X, Y, Z
+
+def _make_orbit_diagram(x, y, z):
+    """
+    Implicit function to obtain the maxima and minima of all 3 dimensions of the
+    attractor.
+
+    Inputs
+    ------
+    x, y, z: np.array
+        np.arrays containing time series of the 3D attractor.
+
+    Returns
+    -------
+    X_max, X_min, Y_max, Y_min, Z_max, Z_min: np.arrays
+        np.arrays containing the local maxima and minima of x, y and z.
+    """
+    X_max = []
+    Y_max = []
+    Z_max = []
+    X_min = []
+    Y_min = []
+    Z_min = []
+
+    # as we evaluate the local maximum and minimum using 5 points, 
+    # we leave out the first and last 2 elements
+    for count in range(2, len(z)-2, 1):
+        # for each index, check if x[count] is the local maximum/minimum, 
+        # and update the previous and current values for all 3 variables
+        if x[count-2] < x[count-1] < x[count] > x[count+1] > x[count+2]:
+            X_max.append(x[count])
+        if x[count-2] > x[count-1] > x[count] < x[count+1] < x[count+2]:
+            X_min.append(x[count])
+        if y[count-2] < y[count-1] < y[count] > y[count+1] > y[count+2]:
+            Y_max.append(y[count])
+        if y[count-2] > y[count-1] > y[count] < y[count+1] < y[count+2]:
+            Y_min.append(y[count])
+        if z[count-2] < z[count-1] < z[count] > z[count+1] > z[count+2]:
+            Z_max.append(z[count])
+        if z[count-2] > z[count-1] > z[count] < z[count+1] < z[count+2]:
+            Z_min.append(z[count])
+
+    return np.array(X_max), np.array(X_min), np.array(Y_max), np.array(Y_min), np.array(Z_max), np.array(Z_min)
+
+def plot_orbit_diagram(R, Z, xlabel = "p1", ylabel = "Z"):
     """
     Plots the orbit diagram made by make_orbit_diagram().
-
     Inputs
     ------
     R: np.array
@@ -93,9 +122,27 @@ def plot_orbit_diagram(R, Z):
     for i in range(len(R)):
         plt.plot(R[i] * np.ones(len(Z[i])), Z[i], 'k.', markersize=1)
     plt.grid(True)
-    plt.xlabel("p1")
-    plt.ylabel("Z")
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.show()
+
+def get_bins(Y):
+    """
+    Bins the values in Y.
+
+    Inputs
+    ------
+    Y: np.array
+        np.array of values to bin
+    
+    Returns
+    -------
+    bins: np.array
+        binned values of Y
+    """
+    bins = np.arange(np.min(Y), np.max(Y) + np.max(Y)*0.01, np.max(Y)*0.01)
+    which_bin = np.digitize(Y, bins)
+    return len(pd.unique(which_bin))
 
 def demo():
     """
@@ -112,7 +159,6 @@ def demo():
         Volume 99, Issue B6
         10 June 1994
         Pages 11859-11877
-
         Inputs
         ------
         x: np.array
@@ -121,7 +167,6 @@ def demo():
             time step
         p1: float
             varying ODE parameter
-
         Returns
         -------
         xdot: np.array
@@ -150,9 +195,9 @@ def demo():
     dR = 10**5 / 2
     R = np.arange(10*10**6, 30*10**6 + dR, dR)
     # time steps to try:
-    t = np.arange(0, 20, 0.001)
+    t = np.arange(0, 50, 0.001)
     # initial conditions:
     x0 = np.array([0, 1.02, 0])
 
     X, Y, Z = make_orbit_diagram(xdot_fun, x0, t, R)
-    plot_orbit_diagram(R, Y)
+    plot_orbit_diagram(R, Y, "p1", "Y")
